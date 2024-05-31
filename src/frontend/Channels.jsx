44f123cedfd88c2cc069a2d67f9585657855452a
Varun Dunna <varun.dunna2524@gmail.com>
2024-05-31 22:19:13 +0530

    Channels-upload

--- src/frontend/Channels.jsx
+++ src/frontend/Channels.jsx
@@ -1,10 +1,18 @@
 import Sidebar from "./Sidebar";
+import "./Channels.css";
 
 export default function Channels() {
   return (
     <div>
       <Sidebar route="channels" />
       <p>Channels</p>
+      <div className="file-input-container">
+        <span className="file-input-label">Choose file</span>
+        <label htmlFor="file-input" className="file-input-button">
+          <span className="upload-icon">&#8645;</span> Upload
+        </label>
+        <input type="file" id="file-input" className="file-input" />
+      </div>
     </div>
   );
 }
